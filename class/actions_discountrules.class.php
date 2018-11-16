b7a6cf9db4b8f7db9974125edc1753892d40e32f
atm-john <john.botella@atm-consulting.fr>
2018-11-16 23:05:02 +0100

    replace name

--- class/actions_discountrules.class.php
+++ class/actions_discountrules.class.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2017 SuperAdmin
+/* Copyright (C) 2018 John BOTELLA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- test/phpunit/discount_rulesFunctionalTest.php
+++ test/phpunit/discount_rulesFunctionalTest.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2007-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2017 SuperAdmin
+ * Copyright (C) 2018 John BOTELLA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
