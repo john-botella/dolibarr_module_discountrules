40172e6b6728e62a3baf173bc3f29e5cde6bac07
ATM john <john.botella@atm-consulting.fr>
2021-02-24 20:34:10 +0100

    Remove mass action btn and clean interface

--- prices_list.php
+++ prices_list.php
@@ -288,7 +288,7 @@ if (empty($reshook))
 	$permissiontoread = $user->rights->{$rightskey}->lire;
 	$permissiontodelete = $user->rights->{$rightskey}->supprimer;
 	$uploaddir = $conf->product->dir_output;
-	include DOL_DOCUMENT_ROOT.'/core/actions_massactions.inc.php';
+//	include DOL_DOCUMENT_ROOT.'/core/actions_massactions.inc.php';
 }
 
 
@@ -514,7 +514,7 @@ if ($resql)
 	);
 	if ($user->rights->{$rightskey}->supprimer) $arrayofmassactions['predelete'] = "<span class='fa fa-trash paddingrightonly'></span>".$langs->trans("Delete");
 	if (in_array($massaction, array('presend', 'predelete'))) $arrayofmassactions = array();
-	$massactionbutton = $form->selectMassAction('', $arrayofmassactions);
+//	$massactionbutton = $form->selectMassAction('', $arrayofmassactions);
 
 	$newcardbutton = '';
 	if ($type === "") $perm = ($user->rights->produit->lire || $user->rights->service->lire);
