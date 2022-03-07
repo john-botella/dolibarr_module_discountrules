d1900d96df7b084e50db2a1507fd34f9327cd609
atm-gregr <81170503+atm-gregr@users.noreply.github.com>
2022-03-07 10:53:53 +0100

    Update sql/update_2.14.0-2.15.0.sql

--- sql/update_2.14.0-2.15.0.sql
+++ sql/update_2.14.0-2.15.0.sql
@@ -14,4 +14,4 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-ALTER TABLE llx_discountrule ADD all_category_project INT(1) NULL DEFAULT '0';
+ALTER TABLE llx_discountrule ADD all_category_project INT(1) NULL DEFAULT 0;
